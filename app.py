38436461b5b93d96ed4157b50b61553f61a0e8bd
DevAhmadYarKhan <210438969+DevAhmadYarKhan@users.noreply.github.com>
2026-08-21 20:07:52 +0100

    Create post /applications route

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
 from dummy_data import applications
+from schemas import ApplicationCreate
+from datetime import datetime, UTC
 
 app = FastAPI()
 
 @app.get("/applications")
 async def get_applications():
     return applications
+
+@app.post("/applications")
+async def create_applications(application: ApplicationCreate):
+
+    new_app = {
+        "id": len(applications) + 1,
+        "company": application.company,
+        "role": application.role,
+        "status": application.status,
+        "job_url": application.job_url,
+        "notes": application.notes,
+        "applied_at": datetime.now(UTC) if application.status != "saved" else None,
+        "created_at": datetime.now(UTC),
+        "updated_at": datetime.now(UTC)
+    }
+
+    applications.append(new_app)
