9f94d895333d4925d609e651bad6089bf9ec723c
DevAhmadYarKhan <210438969+DevAhmadYarKhan@users.noreply.github.com>
2026-08-21 19:18:07 +0100

    Create ApplicationRead pydantic schema

---
+++ schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, Field
+from typing import Literal
+from datetime import datetime
+
+# We will not use this in @app.get("/applications") route just yet because the dummy data uses
+# strings for applied_at etc
+class ApplicationRead(BaseModel):
+    id: int
+    company: str = Field(max_length=100)
+    role: str = Field(max_length=100)
+    status: Literal["saved", "applied", "interview", "offer", "rejected"]
+    job_url: str | None = Field(default=None, max_length=300)
+    notes: str | None = Field(default=None, max_length=500)
+    applied_at: datetime | None = None
+    created_at: datetime
+    updated_at: datetime
