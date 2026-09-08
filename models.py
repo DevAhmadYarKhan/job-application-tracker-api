7b0958e10f24dec235666cc05877a17506049a2d
DevAhmadYarKhan <210438969+DevAhmadYarKhan@users.noreply.github.com>
2026-09-08 04:08:15 +0100

    Add 'Job' SQLModel

---
+++ models.py
@@ -0,0 +1,15 @@
+from sqlmodel import Field, SQLModel, String
+from typing import Literal
+from datetime import datetime
+
+# Repersents a table in database, attributes are column, objects are rows
+class Job(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    company: str = Field(max_length=100)
+    role: str = Field(max_length=100)
+    status: Literal["saved", "applied", "interview", "offer", "rejected"] = Field(sa_type=String)
+    job_url: str | None = Field(default=None, max_length=300)
+    notes: str | None = Field(default=None, max_length=500)
+    applied_at: datetime | None = None
+    created_at: datetime
+    updated_at: datetime
