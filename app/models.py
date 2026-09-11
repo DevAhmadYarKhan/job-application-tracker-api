b02b343b64534d212789f91276df371085039b27
DevAhmadYarKhan <210438969+DevAhmadYarKhan@users.noreply.github.com>
2026-09-11 20:13:18 +0100

    Add password_hash helper functions to User model

--- app/models.py
+++ app/models.py
@@ -2,6 +2,9 @@ from sqlmodel import Field, SQLModel, String, Relationship
 from sqlalchemy import CheckConstraint
 from typing import Literal
 from datetime import datetime, UTC
+from pwdlib import PasswordHash
+
+password_hasher = PasswordHash.recommended()
 
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -12,6 +15,12 @@ class User(SQLModel, table=True):
     updated_at: datetime = Field(default_factory=lambda: datetime.now(UTC))
 
     applications: list["Application"] = Relationship(back_populates="user")
+
+    def set_password(self, password: str) -> None:
+        self.password_hash = password_hasher.hash(password)
+
+    def check_password(self, password: str) -> bool:
+        return password_hasher.verify(password, self.password_hash)
 
 # Repersents a table in database, attributes are column, objects are rows
 class Application(SQLModel, table=True):
