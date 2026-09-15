b94f1a37a31a7426ba2eceecde4718f905821a7f
DevAhmadYarKhan <210438969+DevAhmadYarKhan@users.noreply.github.com>
2026-09-15 02:27:13 +0100

    Add endpoint for signing up

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request, status
 from sqlmodel.ext.asyncio.session import AsyncSession
-from app.schemas import UserRead
+from app.schemas import UserRead, UserCreate
 from app.database import get_session
 from app.services import auth as auth_service
 
@@ -10,3 +10,7 @@ router = APIRouter(tags=["authorization"])
 @router.get("/me", response_model=UserRead)
 async def me(request: Request, db: AsyncSession = Depends(get_session)) -> UserRead:
     return await auth_service.get_current_user(request=request, db=db)
+
+@router.post("/signup", response_model=UserRead, status_code=status.HTTP_201_CREATED)
+async def signup(user: UserCreate, request: Request, db: AsyncSession = Depends(get_session)) -> UserRead:
+    return await auth_service.signup(user=user, request=request, db=db)
--- app/services/auth.py
+++ app/services/auth.py
@@ -1,6 +1,8 @@
 from fastapi import HTTPException, status, Request
 from app.models import User
+from app.schemas import UserCreate
 from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
 
 # Should ideally make this a dependency in other routes to reduce code
@@ -17,3 +19,25 @@ async def get_current_user(request: Request, db: AsyncSession) -> User:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid session")
 
     return user
+
+
+async def signup(user: UserCreate, request: Request, db: AsyncSession) -> User:
+    new_user = User(email=user.email, password_hash="", name=user.name)
+    new_user.set_password(user.password)
+
+    try:
+        db.add(new_user)
+        await db.commit()
+        await db.refresh(new_user)
+
+    except IntegrityError:
+        await db.rollback()
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_CONTENT)
+
+    except SQLAlchemyError:
+        await db.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Database error")
+
+    request.session.clear()
+    request.session["user_id"] = new_user.id
+    return new_user
