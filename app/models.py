ca24d51569b2ecb8deba20f92dfba36bda1ac71a
DevAhmadYarKhan <210438969+DevAhmadYarKhan@users.noreply.github.com>
2026-09-17 00:02:36 +0100

    Fix User SQLModel model

--- app/models.py
+++ app/models.py
@@ -11,8 +11,8 @@ password_hasher = PasswordHash.recommended()
 # Represents a table in database for storing Users, attributes are column, objects are rows
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    email: EmailStr = Field(unique=True, index=True, max_length=255)
-    username: str | None = Field(default=None, max_length=100)
+    email: EmailStr = Field(unique=True, max_length=255)
+    username: str = Field(unique=True, index=True, max_length=100)
     password_hash: str = Field(max_length=255)
     created_at: datetime = Field(default_factory=lambda: datetime.now(UTC))
     updated_at: datetime = Field(default_factory=lambda: datetime.now(UTC))
