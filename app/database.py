52a71f3efb6b6ee8fb78e7677e0b12f63e7554b7
DevAhmadYarKhan <210438969+DevAhmadYarKhan@users.noreply.github.com>
2026-09-21 01:32:48 +0100

    Add alembic migrations

--- app/database.py
+++ app/database.py
@@ -1,15 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine
-from sqlmodel import SQLModel
 from sqlmodel.ext.asyncio.session import AsyncSession
 from app.config import DATABASE_URL
 
 
 engine = create_async_engine(DATABASE_URL)
-
-# This function creates a database with all the tables defined according to SQLModel models
-async def create_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.create_all)
 
 # Gives a session that allows for interacting with the database, we inject it into our routes
 async def get_session():
--- app/main.py
+++ app/main.py
@@ -1,15 +1,7 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
-from app.database import create_db
 from app.routes import applications, auth
 
-# Everything before yield runs at startup, everything after yield runs at shutdown. 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await create_db()
-    yield
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 
 app.include_router(applications.router)
